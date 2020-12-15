ce5536bc7f1597b2efdc195deacccee63243f929
Dustin Coleman <dcoleman@microsoft.com>
2020-12-14 23:16:34 -0800

    Day 15

---
+++ src/AdventOfCode2020/Day15.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    static class Day15
+    {
+        public static int[] Input = new[] { 6, 19, 0, 5, 7, 13, 1 };
+
+        public static void Part1()
+        {
+            long result = Solve(Input, 2020);
+            Debugger.Break();
+        }
+
+        public static void Part2()
+        {
+            long result = Solve(Input, 30000000);
+            Debugger.Break();
+        }
+
+        private static long Solve(int[] input, long turnToSolve)
+        {
+            Dictionary<long, long> lastSpokenByNumber = new Dictionary<long, long>();
+
+            long lastSpoken = 0;
+
+            for (long l = 0; l < input.Length; l++)
+            {
+                lastSpoken = input[l];
+                lastSpokenByNumber[lastSpoken] = l;
+            }
+
+            for (long l = input.Length; l < turnToSolve; l++)
+            {
+                long spoken = 0;
+                if (lastSpokenByNumber.ContainsKey(lastSpoken))
+                {
+                    spoken = l - lastSpokenByNumber[lastSpoken] - 1;
+                }
+
+                lastSpokenByNumber[lastSpoken] = l - 1;
+                lastSpoken = spoken;
+            }
+
+            return lastSpoken;
+        }
+    }
+}
--- src/AdventOfCode2020/Program.cs
+++ src/AdventOfCode2020/Program.cs
@@ -33,8 +33,10 @@ namespace AdventOfCode2020
             //Day12.Part2();
             //Day13.Part1();
             //Day13.Part2();
-            Day14.Part1();
-            Day14.Part2();
+            //Day14.Part1();
+            //Day14.Part2();
+            Day15.Part1();
+            Day15.Part2();
         }
     }
 }
