4cc7e923b251de361101a94d87d0bb90a6e7b0b1
Dustin Coleman <dcoleman@microsoft.com>
2021-11-30 12:13:27 -0800

    Add 2021 Project

---
+++ src/AdventOfCode2021/Day01.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace AdventOfCode2021
+{
+    public class Day01
+    {
+        [Fact]
+        public void Part1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.True(true);
+        }
+    }
+}
