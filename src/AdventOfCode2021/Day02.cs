808f6550be725cbee6b4bbd8b3cc1cb5a25e7b40
Dustin Coleman <dcoleman@microsoft.com>
2021-12-01 21:10:37 -0800

    Day 02

---
+++ src/AdventOfCode2021/Day02.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode2021
+{
+    public class Day02
+    {
+        [Fact]
+        public void Part1()
+        {
+            int depth = 0;
+            int position = 0;
+
+            string[] input = File.ReadAllLines("Day02Input.txt");
+
+            foreach (string line in input)
+            {
+                string[] parts = line.Split(' ');
+                string command = parts[0];
+                int value = Int32.Parse(parts[1]);
+
+                if (command == "up")
+                {
+                    depth -= value;
+                }
+                else if(command == "down")
+                {
+                    depth += value;
+                }
+                else
+                {
+                    position += value;
+                }
+            }
+
+            int result = depth * position;
+
+            Assert.Equal(1989265, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            int depth = 0;
+            int position = 0;
+            int aim = 0;
+
+            string[] input = File.ReadAllLines("Day02Input.txt");
+
+            foreach (string line in input)
+            {
+                string[] parts = line.Split(' ');
+                string command = parts[0];
+                int value = Int32.Parse(parts[1]);
+
+                if (command == "up")
+                {
+                    aim -= value;
+                }
+                else if (command == "down")
+                {
+                    aim += value;
+                }
+                else
+                {
+                    position += value;
+                    depth += aim * value;
+                }
+            }
+
+            long result = depth * position;
+
+            Assert.Equal(2089174012, result);
+        }
+    }
+}
