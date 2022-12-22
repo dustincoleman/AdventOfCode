8eb1c6cc43feed1bcc861e46a143b9ddee21b141
Dustin Coleman <dcoleman@microsoft.com>
2022-12-21 22:18:13 -0800

    Day 20

---
+++ src/AdventOfCode2022/Day20.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+
+namespace AdventOfCode2022
+{
+    public class Day20
+    {
+        [Fact]
+        public void Part1()
+        {
+            LinkedList<long> linkedList = LoadPuzzle();
+            List<LinkedListNode<long>> nodes = new List<LinkedListNode<long>>();
+            LinkedListNode<long> node = linkedList.First;
+
+            while (node != null)
+            {
+                nodes.Add(node);
+                node = node.Next;
+            }
+
+            foreach (LinkedListNode<long> current in nodes)
+            {
+                long toMove = Math.Abs(current.Value) % linkedList.Count;
+                bool forward = (current.Value >= 0);
+
+                if (toMove != 0)
+                {
+                    node = current;
+
+                    if (forward)
+                    {
+                        while (toMove-- > 0)
+                        {
+                            node = node.Next ?? linkedList.First;
+                        }
+
+                        linkedList.Remove(current);
+                        linkedList.AddAfter(node, current);
+                    }
+                    else
+                    {
+                        while (toMove-- > 0)
+                        {
+                            node = node.Previous ?? linkedList.Last;
+                        }
+
+                        linkedList.Remove(current);
+                        linkedList.AddBefore(node, current);
+                    }
+                }
+            }
+
+            List<long> list = linkedList.ToList();
+            int idx = list.IndexOf(0);
+
+            long i1 = list[(1000 + idx) % list.Count];
+            long i2 = list[(2000 + idx) % list.Count];
+            long i3 = list[(3000 + idx) % list.Count];
+
+            long result = i1 + i2 + i3;
+            Assert.Equal(8372, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            LinkedList<long> linkedList = LoadPuzzle(811589153);
+            List<LinkedListNode<long>> nodes = new List<LinkedListNode<long>>();
+            LinkedListNode<long> node = linkedList.First;
+
+            while (node != null)
+            {
+                nodes.Add(node);
+                node = node.Next;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (LinkedListNode<long> current in nodes)
+                {
+                    long toMove = Math.Abs(current.Value) % linkedList.Count;
+                    bool forward = (current.Value >= 0);
+
+                    if (toMove != 0)
+                    {
+                        node = current;
+
+                        if (forward)
+                        {
+                            long toMoveTemp = toMove;
+                            while (toMoveTemp-- > 0)
+                            {
+                                node = node.Next ?? linkedList.First;
+                            }
+
+                            linkedList.Remove(current);
+                            linkedList.AddAfter(node, current);
+                        }
+                        else
+                        {
+                            while (toMove-- > 0)
+                            {
+                                node = node.Previous ?? linkedList.Last;
+                            }
+
+                            linkedList.Remove(current);
+                            linkedList.AddBefore(node, current);
+                        }
+                    }
+                }
+            }
+
+            List<long> list = linkedList.ToList();
+            int idx = list.IndexOf(0);
+
+            long i1 = list[(1000 + idx) % list.Count];
+            long i2 = list[(2000 + idx) % list.Count];
+            long i3 = list[(3000 + idx) % list.Count];
+
+            long result = i1 + i2 + i3;
+            Assert.Equal(0, result);
+        }
+
+        private LinkedList<long> LoadPuzzle(long key = 1)
+        {
+            return new LinkedList<long>(File.ReadAllLines("Day20.txt").Select(x => long.Parse(x) * key));
+        }
+    }
+}
