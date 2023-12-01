cd0dbea70ab5e69b4a08a74fa7df74457dcbc29a
Dustin Coleman <dcoleman@microsoft.com>
2023-12-01 11:43:09 -0800

    Day 01 - Final Cleanup

--- src/AdventOfCode2023/Day01.cs
+++ src/AdventOfCode2023/Day01.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace AdventOfCode2023;
 
 public class Day01
@@ -7,15 +9,16 @@ public class Day01
     [Fact]
     public void Part1()
     {
-        int answer = 0;
-        string[] lines = File.ReadAllLines("Day01.txt");
+        int answer = File.ReadAllLines("Day01.txt").Sum(line => 
+            Decode(line, pos =>
+            {
+                if (char.IsAsciiDigit(line[pos]))
+                {
+                    return line[pos] - '0';
+                }
 
-        foreach(string line in lines)
-        {
-            int first = line.First(char.IsAsciiDigit) - '0';
-            int last = line.Last(char.IsAsciiDigit) - '0';
-            answer += (first * 10) + last;
-        }
+                return null;
+            }));
 
         Assert.Equal(53386, answer);
     }
@@ -23,62 +26,32 @@ public class Day01
     [Fact]
     public void Part2()
     {
-        int answer = 0;
-        string[] lines = File.ReadAllLines("Day01.txt");
+        int answer = File.ReadAllLines("Day01.txt").Sum(line => 
+            Decode(line, pos =>
+            {
+                if (char.IsAsciiDigit(line[pos]))
+                {
+                    return line[pos] - '0';
+                }
 
-        foreach (string line in lines)
-        {
-            int first = FirstPart2Digit(line);
-            int last = LastPart2Digit(line);
-            answer += (first * 10) + last;
-        }
+                for (int i = 0; i < s_numbers.Length; i++)
+                {
+                    if (pos + s_numbers[i].Length <= line.Length && line.IndexOf(s_numbers[i], pos, s_numbers[i].Length) == pos)
+                    {
+                        return i + 1;
+                    }
+                }
+
+                return null;
+            }));
 
         Assert.Equal(53312, answer);
     }
 
-    private int FirstPart2Digit(string line)
+    private int Decode(string line, Func<int, int?> decode)
     {
-        for (int i = 0; i < line.Length; i++)
-        {
-            int? digit = GetPart2Digit(line, i);
-            if (digit.HasValue)
-            {
-                return digit.Value;
-            }
-        }
-
-        throw new Exception("Value not found");
-    }
-
-    private int LastPart2Digit(string line)
-    {
-        for (int i = line.Length - 1; i >= 0; i--)
-        {
-            int? digit = GetPart2Digit(line, i);
-            if (digit.HasValue)
-            {
-                return digit.Value;
-            }
-        }
-
-        throw new Exception("Value not found");
-    }
-
-    private int? GetPart2Digit(string line, int pos)
-    {
-        if (char.IsAsciiDigit(line[pos]))
-        {
-            return line[pos] - '0';
-        }
-
-        for (int i = 0; i < s_numbers.Length; i++)
-        {
-            if (pos + s_numbers[i].Length <= line.Length && line.IndexOf(s_numbers[i], pos, s_numbers[i].Length) == pos)
-            {
-                return i + 1;
-            }
-        }
-
-        return null;
+        int first = Enumerable.Range(0, line.Length).Select(decode).Where(i => i.HasValue).First()!.Value;
+        int last = Enumerable.Range(0, line.Length).Reverse().Select(decode).Where(i => i.HasValue).First()!.Value;
+        return (first * 10) + last;
     }
 }
