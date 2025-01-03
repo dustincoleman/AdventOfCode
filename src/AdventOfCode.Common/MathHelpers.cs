2a87d534e52f33a3b431c03cbf9830685d06e4f3
Dustin Coleman <dcoleman@microsoft.com>
2025-01-02 16:12:41 -0800

    Cleanup

--- src/AdventOfCode.Common/MathHelpers.cs
+++ src/AdventOfCode.Common/MathHelpers.cs
@@ -30,23 +30,11 @@ namespace AdventOfCode.Common
             return a;
         }
 
-        public static BigInteger GreatestCommonDivisor(BigInteger a, BigInteger b)
-        {
-            while (b != 0)
-            {
-                var temp = b;
-                b = a % b;
-                a = temp;
-            }
-
-            return a;
-        }
-
         public static int LeastCommonMultiple(int a, int b) => a / GreatestCommonDivisor(a, b) * b;
         public static int LeastCommonMultiple(this IEnumerable<int> values) => values.Aggregate(LeastCommonMultiple);
         public static long LeastCommonMultiple(long a, long b) => a / GreatestCommonDivisor(a, b) * b;
         public static long LeastCommonMultiple(this IEnumerable<long> values) => values.Aggregate(LeastCommonMultiple);
-        public static BigInteger LeastCommonMultiple(BigInteger a, BigInteger b) => a / GreatestCommonDivisor(a, b) * b;
+        public static BigInteger LeastCommonMultiple(BigInteger a, BigInteger b) => a / BigInteger.GreatestCommonDivisor(a, b) * b;
         public static BigInteger LeastCommonMultiple(this IEnumerable<BigInteger> values) => values.Aggregate(LeastCommonMultiple);
     }
 }
